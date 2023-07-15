30b8591194b13ba877abd4e6f830218f263be01b
mdalamin3091 <alamin@hypescout.co>
2023-07-15 21:56:56 +0600

    fixed something went wrong

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -1,19 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logoutUser } from "../user/userSlice";
+
+const baseQuery = fetchBaseQuery({
+    // baseUrl: "https://personality-typing-tool-backend.vercel.app/api/v1/",
+    baseUrl: "http://localhost:5000/api/v1/",
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+        const auth = localStorage.getItem("auth");
+        if (auth) {
+            const parseAuth = JSON.parse(auth);
+            headers.set("authorization", parseAuth.token);
+        }
+        return headers;
+    },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+    if (result.error && result.error?.data.message === "jwt expired") {
+        api.dispatch(logoutUser());
+    }
+    return result;
+};
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://personality-typing-tool-backend.vercel.app/api/v1/",
-        // baseUrl: "http://localhost:5000/api/v1/",
-        prepareHeaders: async (headers, { getState, endpoint }) => {
-            const auth = localStorage.getItem("auth");
-            if (auth) {
-                const parseAuth = JSON.parse(auth);
-                headers.set("authorization", parseAuth.token)
-            }
-            return headers;
-        }
-    }),
+    baseQuery: baseQueryWithReauth,
     tagTypes: ["character", "cast"],
-    endpoints: (builder) => ({
-    })
-})
+    endpoints: (builder) => ({}),
+});
